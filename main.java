1405bb358838be2f5fc0750b59dd4040d23ada1a
rupsssmit <157969320+rupsssmit@users.noreply.github.com>
2026-07-30 17:18:25 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+package com.insurance.compliancechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplianceCheckerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ComplianceCheckerApplication.class, args);
+    }
+}
